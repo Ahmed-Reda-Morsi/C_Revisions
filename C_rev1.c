fd0a9a334750e20b04c68116238843952d26832b
Ahmed Reda Morsi <ahmed.reda.abdu.alaleem@gmail.com>
2023-01-12 21:30:24 +0200

    make a pointer for function and use it , and use standard types approach to avoid compilers allocation different size

--- C_rev1.c
+++ C_rev1.c
@@ -1,16 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "C_functions.c"
-void PrintMyName(void);
+
+// standard types (each company has it standard types names )
+typedef unsigned char u8;
+typedef signed char   s8;
+typedef unsigned int u16;
+typedef signed int s16;
+typedef float f32;
+typedef double  f64;
+typedef long double f80;
+
 int factorial_number(int num);
 void fibonacci_series(int order);
 void bubble_sort(int *arr, int size);
+int binary_search(const int *arr,int size,int value);
+
+
 int main()
 {   
+
+    int (*ptr_fact)(int)=factorial_number;
     /* PrintM4yName();
-    int num= factorial_number(5);
-    fibonacci_series(4);
      */
-    int arr[50];
+    int num= ptr_fact(5);
+    printf("%x \n",num);
+
+
+    // take array from user.
+    /* int arr[50];
     int size;
     printf(" enter the size of array : ");
     scanf("%d",&size);  
@@ -18,12 +35,18 @@ int main()
     for (int i = 0; i < size; i++)
     {
         scanf(" %d", &arr[i]);
-    }
+    } */
     
+
+    // binary research example:
+    /* int arr[10]={5,55,87,10,32,100,200,9,3,150};
+    int size=10,value=150;
     bubble_sort(arr, size);
     for (int i = 0; i < size; i++)
     {
         printf(" element %d  : %d\n",i+1,arr[i]);
-    }
-    
+    }    
+    printf("index is %d ",binary_search(arr,size,value));
+     */
+
 };
